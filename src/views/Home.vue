407a550b139f423ea7e30454e110edab97bdb970
tina-pina <christina.koenig.de@gmail.com>
2018-12-18 23:10:27 +0100

    Carousel img updated

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,15 +24,19 @@
 
       <!-- Slides with custom text -->
       <b-carousel-slide
+        style="width: 375px; height: 175px"
         img-src="https://images.unsplash.com/photo-1473976345543-9ffc928e648d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1385&q=80"
       ></b-carousel-slide>
 
       <!-- Slides with image only -->
-      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
+      <b-carousel-slide
+        style="width: 375px; height: 175px"
+        img-src="https://images.unsplash.com/photo-1508697371770-529139794f8b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1018&q=80"
+      ></b-carousel-slide>
 
       <!-- Slides with img slot -->
       <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
-      <b-carousel-slide>
+      <!-- <b-carousel-slide>
         <img
           slot="img"
           class="d-block img-fluid w-100"
@@ -41,16 +45,15 @@
           src="https://picsum.photos/1024/480/?image=55"
           alt="image slot"
         >
-      </b-carousel-slide>
-
+      </b-carousel-slide>-->
       <!-- Slide with blank fluid image to maintain slide aspect ratio -->
-      <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
+      <!-- <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
           eros felis, tincidunt a tincidunt eget, convallis vel est. Ut pellentesque
           ut lacus vel interdum.
         </p>
-      </b-carousel-slide>
+      </b-carousel-slide>-->
     </b-carousel>
 
     <div class="container">
